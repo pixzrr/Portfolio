e5c6a32bd2da4b56124d2d695a4913b1b2e9a9e8
pixzrr <91895158+pixzrr@users.noreply.github.com>
2025-03-16 23:14:38 +0100

    Update top.php

--- assets/int/top.php
+++ assets/int/top.php
@@ -1,1 +1,32 @@
-php ici
+<?php
+define('ROOT', '/Portfolio/');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Hey!</title>
+    <meta name="description" content="Mon portfolio">
+    <meta name="author" content="zalken">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php ROOT; ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php ROOT; ?>assets/css/mobile.css">
+</head>
+<script type="text/JavaScript" src="assets/scripts/script.js"></script>
+<body>
+    <header>
+        <a id="logo" href="<?php ROOT; ?>index.html"><img src="<?php ROOT; ?>assets/images/e3.jpg" alt="à remplir"></a> 
+      
+        <p>
+        <a href=#accueil>Accueil</a> 
+        <a href=#projets>Mes projets</a>
+        <a href="../index.html">Accueil</a>
+        </p>
+
+        <div>
+            <a href="https://www.linkedin.com/in/gabriel-portier-210873332/" target="_blank"><img src="<?php ROOT; ?>assets/images/linkedin-2935407_1280.png" alt="à remplir"></a>
+            <a href="https://github.com/pixzrr" target="_blank" id="github"><img src="<?php ROOT; ?>assets/images/github-mark-white.png" alt="à remplir"></a>
+            <a href="mailto:gabriel.portier94@gmail.com"><img src="<?php ROOT; ?>assets/images/letter-2935365_1280.png" alt="à remplir"></a>
+        </div>
+    </header>
