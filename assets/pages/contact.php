91563a86e23a053ad8febb7a794fad4f080fadf5
pixzrr <91895158+pixzrr@users.noreply.github.com>
2025-03-16 23:20:08 +0100

    Update and rename page.html to contact.php

---
+++ assets/pages/contact.php
@@ -0,0 +1,18 @@
+<?php
+if (!empty($_POST['email'])){
+    $email = htmlspecialchars($_POST['email']);
+
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        #création de la requête sql
+        $sql = "INSERT INTO email (email) VALUES ('$email');";
+        #requête pour les doublons
+        $double = "SELECT * FROM email WHERE email = '$email';";
+
+        include '../int/database.php';
+
+        $connexion -> query($sql);
+    } else {
+        echo "L'adresse e-mail n'est pas valide.";
+    }
+}
